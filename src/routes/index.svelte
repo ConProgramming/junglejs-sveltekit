c650ec68e7d05f8cbc27e50573970feb48fb859a
Conner Aldrich <connercaldrich@gmail.com>
2021-03-15 17:27:18 +0000

    preprocessing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,45 +1,22 @@
 <script context="module">
-	export const load = async({ fetch }) => {
-		const query = `
-			query Doubled($x: Int) {
-				double(number: $x)
-			}
-		`;
+  const QUERY = `
+    query Doubled($x: Int) {
+      doubled: double(number: $x)
+    }
+  `;
 
-		const variables = {
-			x: 19,
-		};
-
-		const response = await fetch("/graphql", {
-			body: { query, variables },
-			headers: {
-				"Authorization": "Token ABC123",
-				"Content-Type": "application/json",
-			},
-			method: "POST",
-		});
-
-		const { data, errors } = await response.json();
-
-		if (errors) return {
-			error: new Error(errors.map(({ message }) => message).join("\n")),
-			status: 500,
-		};
-
-		return {
-			props: {
-				doubled: data.double,
-			},
-		};
-	}
+  const VARIABLES = {
+    x: 12
+  }
 </script>
+
 <script>
 	import Counter from '$lib/Counter.svelte';
   export let doubled;
 </script>
 
 <main>
-	<h1>Hello world!</h1>
+	<h1>Hello there!</h1>
 	<h2>The API said {doubled}</h2>
 
 	<Counter />
--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,41 @@
 /** @type {import('@sveltejs/kit').Config} */
+
+const junglePreprocess = {
+  script: async ({ content }) => {
+    if (!content.includes('QUERY')) return { code: content };
+
+    return { code: `
+      ${content}
+
+      export const load = async({ fetch }) => {
+        const response = await fetch("/graphql", {
+          body: { query: QUERY, variables: (VARIABLES ? VARIABLES : {}) },
+          headers: {
+            "Content-Type": "application/json",
+            "cache": "no-cache"
+          },
+          method: "POST",
+        });
+
+        const { data, errors } = await response.json();
+
+        if (errors) return {
+          error: new Error(errors.map(({ message }) => message).join("\\n")),
+          status: 500,
+        };
+
+        return {
+          props: data,
+        };
+      }
+    `};
+  },
+};
+
 module.exports = {
+  preprocess: [
+    junglePreprocess,
+  ],
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
