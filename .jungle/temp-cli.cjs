ad7403996ec5a1cfdea310544dcfa4b54f909fcd
Conner Aldrich <connercaldrich@gmail.com>
2021-03-21 15:18:16 -0500

    Changed: To use easily definable handlers for dataSources

--- .jungle/temp-cli.cjs
+++ .jungle/temp-cli.cjs
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-// RUN WITH: node .jungle/temp-cli.cjs
+const jungleConfig = require(path.join(process.cwd(), 'jungle.config.cjs'));
 
-const jungleConfigImport = require(path.join(process.cwd(), 'jungle.config.cjs'));
-
-const jungleDataPath = path.join(process.cwd(), 'src', 'lib', 'jungle.data.json');
-  
-jungleConfigImport().then(({ dataSources }) => {
-    fs.writeFileSync(jungleDataPath, JSON.stringify(dataSources, undefined, 2)); 
+Promise.all(jungleConfig.dataSources.map(async (source) => {
+    return {
+        ...source,
+        items: await (jungleConfig.handlers[source.format](source.items)),
+    }
+})).then((transformedDataSources) => {
+    const jungleDataPath = path.join(process.cwd(), 'src', 'lib', 'jungle.data.json');
+    fs.writeFileSync(jungleDataPath, JSON.stringify(transformedDataSources, undefined, 2));
 });
--- jungle.config.cjs
+++ jungle.config.cjs
@@ -1,20 +1,20 @@
 const fetch = require('node-fetch');
 
-module.exports = async () => {
-    const bookRes = await fetch('https://openlibrary.org/api/books?bibkeys=ISBN:0201558025,LCCN:93005405&format=json');
-
-    return {
-        dataSources: [
-            {
-                format: "json", name: "book", items: Object.values(await bookRes.json()),
-            },
-            {
-                format: "json", name: "author", items: [
-                    { id: 1, firstName: 'Tom', lastName: 'Coleman' },
-                    { id: 2, firstName: 'Sashko', lastName: 'Stubailo' },
-                    { id: 3, firstName: 'Mikhail', lastName: 'Novikov' },
-                ], queryArgs: { id: 'Int!' },
-            }
-        ]
+module.exports = {
+    dataSources: [
+        {
+            format: "fetch/json", name: "book", items: ['https://openlibrary.org/api/books?bibkeys=ISBN:0201558025,LCCN:93005405&format=json'],
+        },
+        {
+            format: "json", name: "author", items: [
+                { id: 1, firstName: 'Tom', lastName: 'Coleman' },
+                { id: 2, firstName: 'Sashko', lastName: 'Stubailo' },
+                { id: 3, firstName: 'Mikhail', lastName: 'Novikov' },
+            ], queryArgs: { id: 'Int!' },
+        }
+    ],
+    handlers: {
+        "json": (items) => items,
+        "fetch/json": async (items) => Object.values(await (await fetch(...items)).json())
     }
 };
